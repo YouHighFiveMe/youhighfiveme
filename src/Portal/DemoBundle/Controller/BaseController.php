7f3239b6d460d9c10fa95904d6e0a65638fe1aef
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2012-11-22 21:35:02 +0200

    Added DemoBundle, which tries to be a proof of concept bundle showing how the service layer can be utilized to process a form submission

--- src/Portal/DemoBundle/Controller/BaseController.php
+++ src/Portal/DemoBundle/Controller/BaseController.php
@@ -1,9 +1,33 @@
 <?php
 namespace Portal\DemoBundle\Controller;
 
+use \Closure;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Form;
 
 class BaseController extends Controller
 {
+    /**
+     * @param string $path
+     * @param array $parameters
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function createSuccessRedirectResponse($path, array $parameters = array() )
+    {
+        return $this->redirect($this->generateUrl($path, $parameters) );
+    }
 
+    /**
+     * @param Form $form
+     * @param Closure $successCallback
+     * @param Closure $failureFallback
+     * @return mixed
+     */
+    public function processForm(Form $form, Closure $successCallback, Closure $failureFallback = null)
+    {
+        $form->bind($this->getRequest() );
+
+        return $form->isValid() ? $successCallback($form) : $failureFallback($form);
+    }
 }
--- src/Portal/DemoBundle/Controller/DefaultController.php
+++ src/Portal/DemoBundle/Controller/DefaultController.php
@@ -1,10 +1,54 @@
 <?php
 namespace Portal\DemoBundle\Controller;
 
+use Portal\DemoBundle\Form\Model\CatModel;
+
 class DefaultController extends BaseController
 {
     public function indexAction()
     {
-        return $this->render('PortalDemoBundle:Default:index.html.twig');
+        $service    = $this->getCatService();
+        $form       = $service->getCatForm(new CatModel() );
+
+        return $this->render('PortalDemoBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function createAction()
+    {
+        $self       = $this;
+        $service    = $this->getCatService();
+        $form       = $service->getCatForm(new CatModel() );
+
+        return $this->processForm($form, function($form) use($self, $service) {
+
+                $pats = $service->patCatByForm($form);
+                
+                $self->get('session')->setFlash('pats', $pats);
+
+                return $self->createSuccessRedirectResponse('PortalDemoBundle_demo_meoow');
+            },
+            function($form) use($self) {
+                return $self->render('PortalDemoBundle:Default:index.html.twig', array(
+                    'form' => $form->createView(),
+                ));
+            }
+        );
+    }
+
+    public function meoowAction()
+    {
+        return $this->render('PortalDemoBundle:Default:meoow.html.twig', array(
+            'message' => $this->get('session')->getFlash("pats"),
+        ));
+    }
+
+    /**
+     * @return \Portal\DemoBundle\Service\CatService
+     */
+    protected function getCatService()
+    {
+        return $this->get('portal_demo.service.cat');
     }
 }
