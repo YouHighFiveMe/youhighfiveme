0fec8583b3cb5b9ac5de24042e83dda4860ca9ec
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2012-11-22 22:14:30 +0200

    removed newlines

--- src/Portal/DemoBundle/Controller/DefaultController.php
+++ src/Portal/DemoBundle/Controller/DefaultController.php
@@ -24,7 +24,7 @@ class DefaultController extends BaseController
         return $this->processForm($form, function($form) use($self, $service) {
 
                 $pats = $service->patCatByForm($form);
-                
+
                 $self->get('session')->setFlash('pats', $pats);
 
                 return $self->createSuccessRedirectResponse('PortalDemoBundle_demo_meoow');
