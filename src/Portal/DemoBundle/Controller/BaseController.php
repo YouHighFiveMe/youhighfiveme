2daa7473ab06e8f868bd6b66821ca1d8e6f57080
artur-gajewski <artur.gajewski@soprano.fi>
2012-11-26 14:29:21 +0200

    Another suggestion to the service layer and form handling

--- src/Portal/DemoBundle/Controller/BaseController.php
+++ src/Portal/DemoBundle/Controller/BaseController.php
@@ -9,25 +9,13 @@ use Symfony\Component\Form\Form;
 class BaseController extends Controller
 {
     /**
-     * @param string $path
-     * @param array $parameters
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function createSuccessRedirectResponse($path, array $parameters = array() )
-    {
-        return $this->redirect($this->generateUrl($path, $parameters) );
-    }
-
-    /**
      * @param Form $form
-     * @param Closure $successCallback
-     * @param Closure $failureFallback
-     * @return mixed
+     * @return bool
      */
-    public function processForm(Form $form, Closure $successCallback, Closure $failureFallback = null)
+    public function mediateForm(Form $form)
     {
         $form->bind($this->getRequest() );
 
-        return $form->isValid() ? $successCallback($form) : $failureFallback($form);
+        return $form->isValid();
     }
 }
