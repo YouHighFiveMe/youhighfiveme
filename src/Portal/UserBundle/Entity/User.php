e873aa9fd18efde11f9d0dcb357fb58bbf484f63
Artur Gajewski <info@arturgajewski.com>
2013-06-04 12:42:17 +0300

    FOS\UserBundle\Entity\User -> FOS\UserBundle\Model\User

--- src/Portal/UserBundle/Entity/User.php
+++ src/Portal/UserBundle/Entity/User.php
@@ -3,7 +3,7 @@
 
 namespace Portal\UserBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
